eb575dbc034000834007bf3d031a1e6dff712244
Michael2718 <isaevmihail007@gmail.com>
2020-04-13 15:55:37 +0300

    Deleting test file

--- test.py
+++
@@ -1,5 +0,0 @@
-from urllib.request import urlopen
-h = urlopen("https://dofamin.org/index.php?v=ServerFailStat").read().decode("UTF-8")
-h = h[h.index("""<div class="fail-stat__column fail-stat__column_b">Срывов</div>"""):]
-
-print(h)
